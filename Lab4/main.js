05c9219ac3be2e83716e6b3b57a914c0a18398d1
Aidan Pavlik <apavlik@hawk.iit.edu>
2023-11-29 14:55:59 -0600

    Add in 5 locations to choose from

--- Lab4/main.js
+++ Lab4/main.js
@@ -1,31 +1,20 @@
-async function getSunsetSunrise() {
-    const geocodeApiKey = 'YOUR_GEOCODE_API_KEY';
-    const sunriseSunsetApiKey = 'YOUR_SUNRISESUNSET_API_KEY';
-    const searchInput = document.getElementById('searchInput').value;
+const locationsDropdown = document.getElementById('locations');
+const outputLabel = document.getElementById('output');
 
-    try {
-        // Get latitude and longitude from Geocoding API
-        const geocodeResponse = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${searchInput}&key=${geocodeApiKey}`);
-        const geocodeData = await geocodeResponse.json();
+locationsDropdown.addEventListener('change', () => {
+  const selectedLocation = locationsDropdown.value.split(',');
+  const latitude = selectedLocation[0];
+  const longitude = selectedLocation[1];
 
-        const location = geocodeData.results[0].geometry.location;
-        const latitude = location.lat;
-        const longitude = location.lng;
+  const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
 
-        // Get sunrise and sunset times from Sunrise-Sunset API
-        const sunriseSunsetResponse = await fetch(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&formatted=0&date=today&apiKey=${sunriseSunsetApiKey}`);
-        const sunriseSunsetData = await sunriseSunsetResponse.json();
-
-        const sunrise = new Date(sunriseSunsetData.results.sunrise).toLocaleTimeString();
-        const sunset = new Date(sunriseSunsetData.results.sunset).toLocaleTimeString();
-
-        const resultElement = document.getElementById('result');
-        resultElement.innerHTML = `
-            <p>Sunrise: ${sunrise}</p>
-            <p>Sunset: ${sunset}</p>
-        `;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        alert('Error fetching data. Please try again.');
-    }
-}
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Assuming that data.results.sunrise and data.results.sunset are present in the API response
+      outputLabel.innerHTML = `Sunrise: ${data.results.sunrise}<br>Sunset: ${data.results.sunset}`;
+    })
+    .catch(error => {
+      outputLabel.innerHTML = `Error: ${error}`;
+    });
+});
